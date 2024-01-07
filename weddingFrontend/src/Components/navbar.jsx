f83ce45bf21b40f0778143e429331c5b857f1f95
Alec Hannaford <alec322@gmail.com>
2024-01-06 21:16:14 -0800

    nav hover effect

--- weddingFrontend/src/Components/navbar.jsx
+++ weddingFrontend/src/Components/navbar.jsx
@@ -2,17 +2,20 @@ import { Link } from "react-router-dom"
 
 export function NavBar(props){
     return(
-        <nav>
+        <nav className="navContainer">
             <Link
+            className="navLink"
             to = "sandiego"
             >San Diego</Link>
 
             <Link
+            className="navLink"
             to = "seattle"
             >Seattle</Link>
 
 
             <Link
+            className="navLink"
             to = "newyork"
             >New York</Link>
 
