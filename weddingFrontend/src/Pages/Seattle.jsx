c5491939be79529a783ccaeeca1538bc5b2931ce
Alec Hannaford <alec322@gmail.com>
2024-01-18 18:39:57 -0800

    city pages formatted

--- weddingFrontend/src/Pages/Seattle.jsx
+++ weddingFrontend/src/Pages/Seattle.jsx
@@ -4,11 +4,14 @@ function Seattle(props){
     return(
         <div className="fade-in">  
             <div className='detail'>
-                <img id='icon' src="https://cdn-icons-png.flaticon.com/512/535/535239.png"/>
-                <p id='location'>Location: <a
-                href="https://www.google.com/maps/place/5634+12th+Ave+NE,+Seattle,+WA+98105/@47.6704574,-122.3197544,17z/data=!3m1!4b1!4m6!3m5!1s0x5490146446aa1547:0xcdc819fa33b6ed3f!8m2!3d47.6704575!4d-122.3148835!16s%2Fg%2F11c1dp9rfy?entry=ttu"
-                target="_blank"
-                >5634 12th Ave NE</a></p>
+                <p id='location'>
+                    <img id='icon' src="https://cdn-icons-png.flaticon.com/512/535/535239.png"/>
+                    Location: <br id="break"/> 
+                    <a
+                    href="https://www.google.com/maps/place/5634+12th+Ave+NE,+Seattle,+WA+98105/@47.6704574,-122.3197544,17z/data=!3m1!4b1!4m6!3m5!1s0x5490146446aa1547:0xcdc819fa33b6ed3f!8m2!3d47.6704575!4d-122.3148835!16s%2Fg%2F11c1dp9rfy?entry=ttu"
+                    target="_blank">
+                    5634 12th Ave NE</a>
+                </p>
             </div>
             <div className="detail">
                 <img id='icon' src="https://static-00.iconduck.com/assets.00/calendar-small-icon-931x1024-onc7jpcy.png"/>
